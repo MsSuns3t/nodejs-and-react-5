0d8f54f0db448d95d7a53ac449ae7210fb208d62
Alexander Frank <167268313+MsSuns3t@users.noreply.github.com>
2026-09-21 11:43:29 +0200

    added and tested express get endpoint

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const products = [
+    {id:0, name:"bread", price:10.00},
+    {id:1, name:"Cheese", price:15.50},
+    {id:2, name:"Eggs", price:67.67},
+];
+
+app.get('/api/products', (req,res) => {
+    res.send(JSON.stringify(products));
+});
+
+app.listen(3001, () => {
+    console.log("server running on http://localhost:3001");
+});
